6afe2447fdd187119d4b1e5d20f3b8568147afc9
Groismaier Andreas <andreas.groismaier@notrufnoe.at>
2025-10-27 11:54:51 +0100

    yes

--- tdd/guess-random-number/src/GuessTheNumberGame.php
+++ tdd/guess-random-number/src/GuessTheNumberGame.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Kata;
 
-final class ChangeMe
+final class GuessTheNumberGame
 {
     public function changeMe(): bool
     {
--- tdd/guess-random-number/tests/GuessTheNumberGameTest.php
+++ tdd/guess-random-number/tests/GuessTheNumberGameTest.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace KataTests;
 
-use Kata\ChangeMe;
+use Kata\GuessTheNumberGame;
 use PHPUnit\Framework\TestCase;
 
-final class ChangeMeTest extends TestCase
+final class GuessTheNumberGameTest extends TestCase
 {
     public function test_change_me(): void
     {
-        $changeMe = new ChangeMe();
+        $changeMe = new GuessTheNumberGame();
 
         self::assertTrue($changeMe->changeMe());
     }
