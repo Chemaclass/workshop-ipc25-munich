16231dd40f17180b9393b89758d61512c90fcd42
Daniel Becker <beckerd@MacBook-Pro-von-Daniel.local>
2025-10-27 14:39:19 +0100

    test

--- refactoring/parrot/src/AfricanParrot.php
+++ refactoring/parrot/src/AfricanParrot.php
@@ -5,4 +5,16 @@ namespace Parrot;
 final class AfricanParrot extends Parrot
 {
 
+    public function __construct(
+ 
+        private int $numberOfCoconuts,
+        private float $voltage,
+        private bool $isNailed
+    )
+    {
+        
+        perant::__construct();
+
+    }
+
 }
