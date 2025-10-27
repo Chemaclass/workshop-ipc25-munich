46fadaa82b8892ad3f5b934a83e5204d9a3c4995
Marco Berger <m.berger@fiveinfo.ch>
2025-10-27 11:43:02 +0100

    Refactor handsProvider to use iterable and simplify formatting

--- tdd/rock-paper-scissors/tests/RockPaperScissorsTest.php
+++ tdd/rock-paper-scissors/tests/RockPaperScissorsTest.php
@@ -10,24 +10,23 @@ use PHPUnit\Framework\TestCase;
 final class RockPaperScissorsTest extends TestCase
 {
 
-    #[ \PHPUnit\Framework\Attributes\DataProvider('handsProvider') ]
+    #[\PHPUnit\Framework\Attributes\DataProvider('handsProvider')]
     public function testHands(string $playerHand, string $opponentHand, string $expectedResult): void
     {
         $rockPaperScissor = new RockPaperScissor();
         $this->assertEquals($expectedResult, $rockPaperScissor->playHands($playerHand, $opponentHand));
     }
-    public static function handsProvider(): array
+
+    public static function handsProvider(): iterable
     {
-        return [
-            [RockPaperScissor::ROCK, RockPaperScissor::SCISSORS, RockPaperScissor::PLAYER_WINS],
-            [RockPaperScissor::SCISSORS, RockPaperScissor::ROCK, RockPaperScissor::OPPONENT_WINS],
-            [RockPaperScissor::PAPER, RockPaperScissor::ROCK, RockPaperScissor::PLAYER_WINS],
-            [RockPaperScissor::ROCK, RockPaperScissor::PAPER, RockPaperScissor::OPPONENT_WINS],
-            [RockPaperScissor::SCISSORS, RockPaperScissor::PAPER, RockPaperScissor::PLAYER_WINS],
-            [RockPaperScissor::PAPER, RockPaperScissor::SCISSORS, RockPaperScissor::OPPONENT_WINS],
-            [RockPaperScissor::PAPER, RockPaperScissor::PAPER, RockPaperScissor::DRAW],
-            [RockPaperScissor::SCISSORS, RockPaperScissor::SCISSORS, RockPaperScissor::DRAW],
-            [RockPaperScissor::ROCK, RockPaperScissor::ROCK, RockPaperScissor::DRAW],
-        ];
+        yield [RockPaperScissor::ROCK, RockPaperScissor::SCISSORS, RockPaperScissor::PLAYER_WINS];
+        yield [RockPaperScissor::SCISSORS, RockPaperScissor::ROCK, RockPaperScissor::OPPONENT_WINS];
+        yield [RockPaperScissor::PAPER, RockPaperScissor::ROCK, RockPaperScissor::PLAYER_WINS];
+        yield [RockPaperScissor::ROCK, RockPaperScissor::PAPER, RockPaperScissor::OPPONENT_WINS];
+        yield [RockPaperScissor::SCISSORS, RockPaperScissor::PAPER, RockPaperScissor::PLAYER_WINS];
+        yield [RockPaperScissor::PAPER, RockPaperScissor::SCISSORS, RockPaperScissor::OPPONENT_WINS];
+        yield [RockPaperScissor::PAPER, RockPaperScissor::PAPER, RockPaperScissor::DRAW];
+        yield [RockPaperScissor::SCISSORS, RockPaperScissor::SCISSORS, RockPaperScissor::DRAW];
+        yield [RockPaperScissor::ROCK, RockPaperScissor::ROCK, RockPaperScissor::DRAW];
     }
 }
