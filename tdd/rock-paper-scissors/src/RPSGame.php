222c3d51ac63d2c45ce8242f6577978dbded64bc
scheichf <felix.scheichelbauer@notrufnoe.at>
2025-10-27 11:43:49 +0100

    :white_check_mark: Refactor RPSGame condition formatting for readability and update test file formatting

--- tdd/rock-paper-scissors/src/RPSGame.php
+++ tdd/rock-paper-scissors/src/RPSGame.php
@@ -8,15 +8,16 @@ final class RPSGame
 {
     public function play(string $player1, string $player2): string
     {
-        if(
+        if (
             ($player1 === 'rock' && $player2 === 'scissors')
-            || ($player1 === 'paper' && $player2 === 'rock') || ($player1 === 'scissors' && $player2 === 'paper')
-        )
-        {
+            || ($player1 === 'paper' && $player2 === 'rock')
+            || ($player1 === 'scissors' && $player2 === 'paper')
+        ) {
             return 'player1';
         }
-        if(($player1 === 'scissors' && $player2 === 'rock') || ($player1 === 'rock' && $player2 === 'paper') || ($player1 === 'paper' && $player2 === 'scissors'))
-        {
+        if (($player1 === 'scissors' && $player2 === 'rock')
+            || ($player1 === 'rock' && $player2 === 'paper')
+            || ($player1 === 'paper' && $player2 === 'scissors')) {
             return 'player2';
         }
 
--- tdd/rock-paper-scissors/tests/ChangeMeTest.php
+++ tdd/rock-paper-scissors/tests/ChangeMeTest.php
@@ -59,4 +59,5 @@ final class ChangeMeTest extends TestCase
             ['rock', 'paper', 'player2'],
         ];
     }
+
 }
