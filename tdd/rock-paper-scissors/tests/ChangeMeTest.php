e2cbe28f11792ff6e04c057e5e2f8b7c71285e48
Groismaier Andreas <andreas.groismaier@notrufnoe.at>
2025-10-27 11:39:18 +0100

    :white_check_mark

--- tdd/rock-paper-scissors/tests/ChangeMeTest.php
+++ tdd/rock-paper-scissors/tests/ChangeMeTest.php
@@ -21,15 +21,29 @@ final class ChangeMeTest extends TestCase
     public static function dataProvider(): array
     {
         return [
-            ['rock', 'rock', 'draw'],
             ['paper', 'paper', 'draw'],
             ['scissors', 'scissors', 'draw'],
-            ['rock', 'scissors', 'player1'],
             ['scissors', 'rock', 'player2'],
             ['paper', 'rock', 'player1'],
-            ['rock', 'paper', 'player2'],
             ['scissors', 'paper', 'player1'],
             ['paper', 'scissors', 'player2'],
+        ];
+    }
+
+
+    #[DataProvider('rockProvider')]
+    public function test_rock(string $player1, string $player2, string $expected): void
+    {
+        $game = new RPSGame();
+        self::assertEquals($expected, $game->play($player1, $player2));
+    }
+
+    public static function rockProvider(): array
+    {
+        return [
+            ['rock', 'rock', 'draw'],
+            ['rock', 'scissors', 'player1'],
+            ['rock', 'paper', 'player2'],
         ];
     }
 }
