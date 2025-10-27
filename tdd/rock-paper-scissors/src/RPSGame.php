1692f81df44a7894fcab6c5a250615a8e8589335
scheichf <felix.scheichelbauer@notrufnoe.at>
2025-10-27 11:45:27 +0100

    Refactor RPSGame condition formatting for consistency

--- tdd/rock-paper-scissors/src/RPSGame.php
+++ tdd/rock-paper-scissors/src/RPSGame.php
@@ -17,7 +17,8 @@ final class RPSGame
         }
         if (($player1 === 'scissors' && $player2 === 'rock')
             || ($player1 === 'rock' && $player2 === 'paper')
-            || ($player1 === 'paper' && $player2 === 'scissors')) {
+            || ($player1 === 'paper' && $player2 === 'scissors')
+        ) {
             return 'player2';
         }
 
