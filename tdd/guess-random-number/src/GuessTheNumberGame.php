6450a9b23d88fadecc49b676fddd528fdce290ba
Groismaier Andreas <andreas.groismaier@notrufnoe.at>
2025-10-27 12:19:49 +0100

    yes

--- tdd/guess-random-number/src/GuessTheNumberGame.php
+++ tdd/guess-random-number/src/GuessTheNumberGame.php
@@ -6,16 +6,27 @@ namespace Kata;
 
 final class GuessTheNumberGame
 {
+    private int $guessesCount = 0;
+
     public function __construct(private readonly int $number)
     {
     }
 
     public function guess(int $guessingNumber): string
     {
+        $this->guessesCount++;
+
         if($guessingNumber === $this->number  ) {
             return "You win!";
         }
-//        if($)
+
+        if($this->guessesCount >= 3) {
+            return "You lose!";
+        }
+
+        if($guessingNumber < $this->number) {
+            return 'higher';
+        }
         return 'lower';
     }
 }
--- tdd/guess-random-number/tests/GuessTheNumberGameTest.php
+++ tdd/guess-random-number/tests/GuessTheNumberGameTest.php
@@ -16,15 +16,23 @@ final class GuessTheNumberGameTest extends TestCase
         self::assertEquals("You win!", $guessTheNumberGame->guess(5));
     }
 
-    public function testWinOnMultipleGuesses(): void
+    public function testGuessLower(): void
     {
         $guessTheNumberGame = new GuessTheNumberGame(5);
-        self::assertEquals("lower", $guessTheNumberGame->guess(3));
+        self::assertEquals("lower", $guessTheNumberGame->guess(8));
     }
 
-    public function testWinOnMultipleGuesses(): void
+    public function testGuessHigher(): void
     {
         $guessTheNumberGame = new GuessTheNumberGame(5);
-        self::assertEquals("lower", $guessTheNumberGame->guess(3));
+        self::assertEquals("higher", $guessTheNumberGame->guess(3));
+    }
+
+    public function testMultipleGuess(): void
+    {
+        $guessTheNumberGame = new GuessTheNumberGame(5);
+        self::assertEquals("lower", $guessTheNumberGame->guess(8));
+        self::assertEquals("higher", $guessTheNumberGame->guess(3));
+        self::assertEquals("You lose!", $guessTheNumberGame->guess(4));
     }
 }
