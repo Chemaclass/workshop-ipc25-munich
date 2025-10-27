69f40384f05b1c102353b19f623b66a0d57d96d5
Nils <nils.schmidt@mail-von-mir.de>
2025-10-27 15:46:28 +0100

    change player name

--- refactoring/tennis/src/TennisGame1.php
+++ refactoring/tennis/src/TennisGame1.php
@@ -18,7 +18,7 @@ class TennisGame1 implements TennisGame
 
     public function wonPoint(string $playerName): void
     {
-        if ($playerName === 'player1') {
+        if ($playerName === $this->player1Name) {
             $this->player1Score++;
         } else {
             $this->player2Score++;
