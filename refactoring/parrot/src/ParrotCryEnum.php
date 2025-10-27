6c02484ed56d7e2368727f3cfb3facd116e655bb
Tomaso Minelli <minni@osti.co>
2025-10-27 14:28:21 +0100

    stuff

--- refactoring/parrot/src/ParrotCryEnum.php
+++ refactoring/parrot/src/ParrotCryEnum.php
@@ -11,8 +11,8 @@ namespace Parrot;
  */
 Enum ParrotCryEnum
 {
-    public const int EUROPEAN = 'Sqoork!';
-    public const int AFRICAN = 'Sqaark!';
-    public const int NORWEGIAN_BLUE_VOLT_0 = 'Bzzzzzz';
-    public const int NORWEGIAN_BLUE_VOLT_N = '...';
+    public const string EUROPEAN = 'Sqoork!';
+    public const string AFRICAN = 'Sqaark!';
+    public const string NORWEGIAN_BLUE_VOLT_0 = 'Bzzzzzz';
+    public const string NORWEGIAN_BLUE_VOLT_N = '...';
 }
