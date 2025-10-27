b1ff7eb0c7b79cc628497ca03042fd2a5b6cd76c
Marco Berger <m.berger@fiveinfo.ch>
2025-10-27 15:46:09 +0100

    Refactor `getScore` method in `TennisGame1` to improve readability and maintainability by extracting logic into dedicated helper methods.

--- refactoring/tennis/src/TennisGame1.php
+++ refactoring/tennis/src/TennisGame1.php
@@ -28,48 +28,15 @@ class TennisGame1 implements TennisGame
 
     public function getScore(): string
     {
-        $score = '';
-        if ($this->scorePlayer1 === $this->scorePlayer2) {
-            $score = $this->getStr();
-        } elseif ($this->scorePlayer1 >= 4 || $this->scorePlayer2 >= 4) {
-            $minusResult = $this->scorePlayer1 - $this->scorePlayer2;
-            if ($minusResult === 1) {
-                $score = 'Advantage player1';
-            } elseif ($minusResult === -1) {
-                $score = 'Advantage player2';
-            } elseif ($minusResult >= 2) {
-                $score = 'Win for player1';
-            } else {
-                $score = 'Win for player2';
-            }
-        } else {
-            for ($i = 1; $i < 3; $i++) {
-                if ($i === 1) {
-                    $tempScore = $this->scorePlayer1;
-                } else {
-                    $score .= '-';
-                    $tempScore = $this->scorePlayer2;
-                }
-                switch ($tempScore) {
-                    case 0:
-                        $score .= 'Love';
-                        break;
-                    case 1:
-                        $score .= 'Fifteen';
-                        break;
-                    case 2:
-                        $score .= 'Thirty';
-                        break;
-                    case 3:
-                        $score .= 'Forty';
-                        break;
-                }
-            }
+        if ($this->isTie()) {
+            return $this->getStandingNameSamePoint();
+        } elseif ($this->isAdvantage()) {
+            return $this->getStandingNameAdvantage();
         }
-        return $score;
+        return  $this->getStandingNameRegular('');
     }
 
-    public function getStr(): string
+    public function getStandingNameSamePoint(): string
     {
         $score = match ($this->scorePlayer1) {
             0 => 'Love-All',
@@ -78,5 +45,70 @@ class TennisGame1 implements TennisGame
             default => 'Deuce',
         };
         return $score;
+    }
+
+    /**
+     * @return string
+     */
+    private function getStandingNameAdvantage(): string
+    {
+        $minusResult = $this->scorePlayer1 - $this->scorePlayer2;
+        if($minusResult === 1) {
+            $score = 'Advantage player1';
+        } elseif($minusResult === -1) {
+            $score = 'Advantage player2';
+        } elseif($minusResult >= 2) {
+            $score = 'Win for player1';
+        } else {
+            $score = 'Win for player2';
+        }
+        return $score;
+    }
+
+    /**
+     * @param string $score
+     * @return string
+     */
+    private function getStandingNameRegular(string $score): string
+    {
+        for($i = 1; $i < 3; $i++) {
+            if($i === 1) {
+                $tempScore = $this->scorePlayer1;
+            } else {
+                $score .= '-';
+                $tempScore = $this->scorePlayer2;
+            }
+            switch($tempScore) {
+                case 0:
+                    $score .= 'Love';
+                    break;
+                case 1:
+                    $score .= 'Fifteen';
+                    break;
+                case 2:
+                    $score .= 'Thirty';
+                    break;
+                case 3:
+                    $score .= 'Forty';
+                    break;
+            }
+        }
+        return $score;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isTie(): bool
+    {
+        return $this->scorePlayer1 === $this->scorePlayer2;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isAdvantage(): bool
+    {
+        return $this->scorePlayer1 >= 4 || $this->scorePlayer2 >= 4;
     }
 }
