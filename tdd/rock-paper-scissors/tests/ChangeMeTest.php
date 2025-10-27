aaecd81696671043cb49544c4d1bf479edc10939
Groismaier Andreas <andreas.groismaier@notrufnoe.at>
2025-10-27 11:18:30 +0100

    :white_check_mark

--- tdd/rock-paper-scissors/tests/ChangeMeTest.php
+++ tdd/rock-paper-scissors/tests/ChangeMeTest.php
@@ -14,7 +14,18 @@ final class ChangeMeTest extends TestCase
     public function test_play_rock_rock(): void
     {
         $game = new RPSGame();
-
         self::assertEquals('draw', $game->play('rock', 'rock'));
+    }
+
+    public function test_play_paper_paper(): void
+    {
+        $game = new RPSGame();
+        self::assertEquals('draw', $game->play('paper', 'paper'));
+    }
+
+    public function test_play_scissors_scissors(): void
+    {
+        $game = new RPSGame();
+        self::assertEquals('draw', $game->play('scissors', 'scissors'));
     }
 }
