a67e6a9fd6686e799a2696fed093a8dca0d32727
Stanislav Seifert <stanislav.seifert@hartje.de>
2025-10-27 14:45:12 +0100

    add european parrot

---
+++ refactoring/parrot/src/EuropeanParrot.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Parrot;
+
+use Exception;
+
+class EuropeanParrot extends Parrot
+{
+
+    /**
+     * @throws Exception
+     */
+    public function getSpeed(): float
+    {
+        return parent::BASE_SPEED;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function getCry(): string
+    {
+        return 'Sqoork!';
+    }
+
+}
